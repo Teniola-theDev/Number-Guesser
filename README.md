14415e39f9604395fc41f1285a1592365c735e7e
Teniola Olowookere <90247181+Teniola-theDev@users.noreply.github.com>
2022-08-30 14:21:43 +0100

    added simple validation

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 A short game from the Brad Traversy Javascript course.
 We will be coding the javascript ourselves.
+
+## Issues
+
+- I could not see the values of my span after setting what should be there in javascript
+
+  ```
+    let min = 1,
+     max = 10,
+    const maxNum = document.querySelector('.max-num');
+    maxNum.textContent = max;
+    //NOT
+    maxNum.value = max;
+
+  ```
+
+  -
+
+  * Cannot set property 'color' of Undefined in JS
+    - I was doing
+    ```
+       message.textContent.style.color = color;
+       //INSTEAD OF
+       message.style.color = color;
+    ```
+  * To define a color as a parameter, add it in quotes
+
+````
+   function setMessage(error, color) {
+     message.style.color = color;
+     message.textContent = error;
+   }
+   setMessage('Kindly input a number between 1 and 10', 'red');
+   ```
+
+````
--- app.js
+++ app.js
@@ -10,5 +10,28 @@ const maxNum = document.querySelector('.max-num');
 const guessInput = document.querySelector('#guess-input');
 const guessBtn = document.querySelector('#guess-btn');
 const message = document.querySelector('.message');
-//set validation
-//check if winning or losing
+
+minNum.textContent = min;
+maxNum.textContent = max;
+// add event listener
+
+guessBtn.addEventListener('click', function () {
+  guess = parseInt(guessInput.value);
+  console.log(guess);
+
+  //set validation
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage('Kindly input a number between 1 and 10', 'red');
+    console.log(guessInput.value);
+  }
+  //check if winning or losing
+  if (guess === winningNum) {
+    guessInput.disabled = true;
+    guessInput.style.borderColor = 'green';
+    setMessage(`${winningNum} is correct. YOU HAVE WON!`, 'green');
+  }
+});
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
